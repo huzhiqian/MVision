c9cc214f02db6505cc17eddf4bcec59bdd38e711
胡志乾 <814397842@qq.com>
2019-01-17 13:39:14 +0800

    fixed some bug

--- 稳定版/HalconMVision/Halcon.MVision.Controls/HalAcqFifoEdit.cs
+++ 稳定版/HalconMVision/Halcon.MVision.Controls/HalAcqFifoEdit.cs
@@ -121,7 +121,6 @@ namespace Halcon.MVision.Controls
                     string result = GetCameraBoardInfo(item, "device");
                     if (result != string.Empty)
                         camList.Add(result);
-                    break;
                 }
                 return camList;
             }
